2e742c1c4d1a908c0607d70a6730983fa9deba69
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-06-23 15:44:11 +0600

    gag

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SimpleCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Emon Is Here");
 
             
             
