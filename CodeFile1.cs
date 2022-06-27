d54832c482a142078adf1bf5ac5c42c2381b943c
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-06-27 18:02:35 +0600

    t

---
+++ CodeFile1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Emon
+{
+    class emon
+    {
+        private static void Main(string[] arg)
+        {
+            int myNum = 21;
+            Console.WriteLine(myNum);
+        }
+    }
+}
