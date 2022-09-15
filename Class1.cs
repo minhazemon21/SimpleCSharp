3cc4f1f33032847413ea3b8ffed67cabb754fa4d
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-09-15 16:40:57 +0600

    pyramid in c#

--- Class1.cs
+++ Class1.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace SimpleCSharp
-{
-    class Car
-    {
-        string model;
-        string Owner;
-        int year;
+//namespace SimpleCSharp
+//{
+//    class Car
+//    {
+//        string model;
+//        string Owner;
+//        int year;
 
-        static void Main(string[] arg)
-        {
-            Car Toyota = new Car();
-            Toyota.model = "star";
-            Toyota.Owner = "Emon";
-            Toyota.year = 2025;
+//        static void Main(string[] arg)
+//        {
+//            Car Toyota = new Car();
+//            Toyota.model = "star";
+//            Toyota.Owner = "Emon";
+//            Toyota.year = 2025;
 
 
-            Car Ford = new Car();
-            Ford.model = "Mustang";
-            Ford.Owner = "Emon";
-            Ford.year = 2024;
+//            Car Ford = new Car();
+//            Ford.model = "Mustang";
+//            Ford.Owner = "Emon";
+//            Ford.year = 2024;
 
-            Console.WriteLine(Ford.Owner);
-        }
+//            Console.WriteLine(Ford.Owner);
+//        }
 
-    }
-}
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 namespace SimpleCSharp
 {
     class Program
@@ -7,53 +9,114 @@ namespace SimpleCSharp
         string color = "red";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var i = 0;
-            for(i=0; i<=10;i++)
-            {
-                Console.WriteLine("Emon Is Here");
 
+            //string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            //Console.Write("\nLINQ : Display the name of the days of a week : ");
+            //Console.Write("\n------------------------------------------------\n");
+
+
+            //var days = from WeekDay in dayWeek
+            //           select WeekDay;
+            //foreach (var WeekDay in days)
+            //{
+            //    Console.WriteLine(WeekDay);
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Hello World!");
+            //var i = 0;
+            //for(i=0; i<=10;i++)
+            //{
+            //    Console.WriteLine("Emon Is Here");
+
+            //}
+
+            //Program myobject = new Program();
+            //for(i=0; i<=10; i++)
+            //{
+            //    Console.WriteLine(myobject.color);
+
+            //}
+
+            //Console.WriteLine("What is your Name: ");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("Your Age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your Information is: " + userName + "My age is: " + age);
+
+
+            //Console.WriteLine("Calculator");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = a + b;
+            //Console.WriteLine("Addition Is: " + c);
+            //int d = a - b;
+            //Console.WriteLine("Subtraction Is: " + d);
+            //int e = a * b;
+            //Console.WriteLine("Multiplication Is: " + e);
+            //int f = a / b;
+            //Console.WriteLine("Division Is: " + f);
+            //int g = a % b;
+            //Console.WriteLine("Modulus Is: " + g);
+            //int h = a++;
+            //Console.WriteLine("Incriment Is: " + h);
+            //int j = --b;
+            //Console.WriteLine("Decrement Is: " + j);
+            //int k = ++a;
+            //Console.WriteLine("Incriment Is: " + k);
+
+            //Console.WriteLine("Calculator");
+            //Console.WriteLine("Minimum value " + Math.Min(a, b));
+            //Console.WriteLine("Absulate vale " + Math.Abs(-105));
+            //Console.WriteLine("Square value " + Math.Sqrt(a));
+            //Console.WriteLine("Round value " + Math.Round(15.32));
+
+
+            //int numberoflayer = 6, Space, Number;
+            //Console.WriteLine("Print paramid");
+            //for (int i = 1; i <= numberoflayer; i++) // Total number of layer for pramid  
+            //{
+            //    for (Space = 1; Space <= (numberoflayer - i); Space++) // Loop For Space  
+            //        Console.Write(" ");
+            //    for (Number = 1; Number <= i; Number++) //increase the value  
+            //        Console.Write('*');
+            //    for (Number = (i - 1); Number >= 1; Number--) //decrease the value  
+            //        Console.Write('*');
+            //    Console.WriteLine();
+            //}
+
+
+            Console.WriteLine("Pyramid in C#");
+
+            int layer = Convert.ToInt32(Console.ReadLine()), space, star;
+            for (int intial = 0; intial <= layer; intial++) //user for layer
+            {
+                for (space = 1; space <= (layer - intial); space++) // user for space
+
+                    Console.Write(" ");
+
+                for (star = 1; star <= intial; star++)
+
+                    Console.Write("*");
+
+                for (star = (intial - 1); star >= 1; star--)
+
+                    Console.Write("*");
+
+
+
+                Console.WriteLine();
             }
 
-            Program myobject = new Program();
-            for(i=0; i<=10; i++)
-            {
-                Console.WriteLine(myobject.color);
-
-            }
-
-            Console.WriteLine("What is your Name: ");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Your Age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your Information is: " + userName + "My age is: " + age);
 
 
-            Console.WriteLine("Calculator");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = a + b;
-            Console.WriteLine("Addition Is: " + c);
-            int d = a - b;
-            Console.WriteLine("Subtraction Is: " + d);
-            int e = a * b;
-            Console.WriteLine("Multiplication Is: " + e);
-            int f = a / b;
-            Console.WriteLine("Division Is: " + f);
-            int g = a % b;
-            Console.WriteLine("Modulus Is: " + g);
-            int h = a++;
-            Console.WriteLine("Incriment Is: " + h);
-            int j = --b;
-            Console.WriteLine("Decrement Is: " + j);
-            int k = ++a;
-            Console.WriteLine("Incriment Is: " + k);
+            Console.WriteLine(layer);
 
-            Console.WriteLine("Calculator");
-            Console.WriteLine("Minimum value " + Math.Min(a, b));
-            Console.WriteLine("Absulate vale " + Math.Abs(-105));
-            Console.WriteLine("Square value " + Math.Sqrt(a));
-            Console.WriteLine("Round value " + Math.Round(15.32));
+
+
+
+
+
 
 
         }
